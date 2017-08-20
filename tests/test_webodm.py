0ca25d9d3efdf8923b14cc204c211992c95f8834
Unknown <rmallermartins@gmail.com>
2017-08-20 07:19:19 -0700

    Fake test

---
+++ tests/test_webodm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
