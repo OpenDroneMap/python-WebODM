af7504343a9dedd50da602057239b32bfec5d932
Unknown <rmallermartins@gmail.com>
2017-08-21 20:48:08 -0300

    Client code organization

--- webodm/client.py
+++ webodm/client.py
@@ -8,29 +8,28 @@ LOCAL_HOST = 'http://localhost:8000'
 class Webodm(object):
 
     def __init__(self, username=None, password=None, host=LOCAL_HOST):
-        self.token = None
-        self.host = host
-        
         if username is not None and password is None:
             AttributeError('Username passed, but password is missing.')
         elif username is None and password is not None:
             AttributeError('Password passed, but username is missing.')
 
+        self.token = None
+        self.host = host
+
         if username is not None and password is not None:
             self.authenticate(username, password)
 
     def authenticate(self, username, password):
-        resp = requests.post(
-            '{}{}'.format(self.host, '/api/token-auth/'),
-            data={
-                'username': username,
-                'password': password
-            })
+        url = '{}{}'.format(self.host, '/api/token-auth/')
+        req_data = {
+            'username': username,
+            'password': password
+        }
+        resp = requests.post(url, data=req_data)
 
         data = resp.json()
-        if resp.status_code == 400:
-            if 'non_field_errors' in data:
-                errors = " ".join(data.get('non_field_errors'))
-                raise NonFieldErrors(errors, response=resp)
+        if resp.status_code == 400 and 'non_field_errors' in data:
+            errors = " ".join(data.get('non_field_errors'))
+            raise NonFieldErrors(errors, response=resp)
 
         self.token = data.get('token')
